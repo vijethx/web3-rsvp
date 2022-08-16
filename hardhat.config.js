85f616252cf590d724b9c175f4d702db90e21ef0
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2022-08-16 14:34:03 +0530

    deployed to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 80001,
+    },
+  },
 };
